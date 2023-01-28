84a1f7c5e3e8f80432b01f45a1db7b3b18accb42
MikeO <10thmountain@zoho.com>
2023-01-28 20:03:14 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+/*
+
+  "use_rpc": true,
+  // getnetworkhashps: Returns the estimated network hashes per second. This should be a positive whole number
+  "getnetworkhashps": "getnetworkhashps",
+  // getmininginfo: Returns a json object containing mining-related information
+  "getmininginfo": "getmininginfo",
+  // getdifficulty: Returns the proof-of-work difficulty as a multiple of the minimum difficulty. This should be a positive whole or decimal number
+  "getdifficulty": "getdifficulty",
+  // getconnectioncount: Returns the number of connections to other nodes. This should be a positive whole number
+  "getconnectioncount": "getconnectioncount",
+  // getblockcount: Returns the number of blocks in the longest blockchain. This should be a positive whole number
+  "getblockcount": "getblockcount",
+  // getblockhash: Returns hash of block in best-block-chain at height provided. This should be a string value
+  "getblockhash": "getblockhash",
+  // getblock: Returns an object with information about a particular block
+  "getblock": "getblock",
+  // getrawtransaction: Returns raw transaction data. Can return a hex-encoded string that is serialized or an object with txid information depending on the decrypt value (0 = false or 1 = true)
+  "getrawtransaction": "getrawtransaction",
+  // getinfo: Returns an object containing various state info
+  "getinfo": "getinfo",
+  // getblockchaininfo: Returns an object containing various state info regarding blockchain processing
+  "getblockchaininfo": "getblockchaininfo",
+  // getpeerinfo: Returns data about each connected network node as a json array of objects
+  "getpeerinfo": "getpeerinfo",
+  // gettxoutsetinfo: Returns an object with statistics about the unspent transaction output set
+  "gettxoutsetinfo": "gettxoutsetinfo",
+  // getvotelist: Returns an object with details regarding the current vote list
+  "getvotelist": "masternodelist votes",
+  // getmasternodecount: Returns a json object containing the total number of masternodes on the network
+  "getmasternodecount": "getmasternodecount",
+  // getmasternodelist: Returns a json array containing status information for all masternodes on the network
+  "getmasternodelist": "listmasternodes",
+  // verifymessage: Verify a signed message. Must accept the following arguments:
+  //                address: The wallet address to use for the signature
+  //                signature: The signature provided by the signer in base 64 encoding
+  //                message: The message that was signed
+  "verifymessage": "verifymessage"
+
+*/
